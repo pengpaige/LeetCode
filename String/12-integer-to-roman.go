cc60bee4abc822a84cab1140f1df2acbf6fe9091
pengpaige <pengpaige1@gmail.com>
2020-05-02 15:54:07 +0800

    20200502

--- String/12-integer-to-roman.go
+++ String/12-integer-to-roman.go
@@ -4,6 +4,7 @@ func intToRoman(num int) string {
     nums := []int{     1000, 900, 500, 400,  100, 90,   50,  40,   10,   9,    5,   4,    1}
     romans := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
     ans, index := "", 0
+    // 因为测试例的局限性，官方的输入最大不超过 3999，所以这道题的解法是比较取巧的
     for index < 13 {
         for num >= nums[index] {
             ans += romans[index]
