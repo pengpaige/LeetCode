1277e21663394a74f40b688e3be12a247f41a676
pengbo1 <pengbo1@jd.com>
2020-05-12 01:56:39 +0800

    20200512

--- Math/357.count-numbers-with-unique-digits.go
+++ Math/357.count-numbers-with-unique-digits.go
@@ -5,30 +5,37 @@
  */
 
 // @lc code=start
+
+
+// 搞懂递归的难度仅次于搞懂女朋友的脑回路
+
+// 回溯或者 DFS 很多时候很难统一成一个入口
+// 这时候尝试遍历初始条件将 “第一层” DFS 写成多个入口比较容易理解
 func countNumbersWithUniqueDigits(n int) int {
 	var ans int
+    mem := make(map[int]bool, 0)
     for i := 1; i < 10; i++ {
-		mem := make(map[int]bool, 10)
-		mem[i] = true
-		ans += dfs(i, 1, n, mem)
+		dfs(i, 1, n, &mem, &ans)
 	}
+    ans++
 	return ans
 }
 
 
-func dfs(currNum, step, n int, mem map[int]bool) int {
+func dfs(d, step, n int, mem *map[int]bool, count *int) {
 	if step > n {
-		return 0
+		return
 	}
-	var count int
+    if (*mem)[d] {
+        return
+    }
+    (*mem)[d] = true
+    (*count)++
 	for i := 0; i < 10; i++ {
-		if mem[i] {
-			continue
-		}
-		mem[i] = true
-		count += dfs(currNum*10+i, step+1, n, mem)
+		dfs(i, step+1, n, mem, count)
 	}
-	return count
+	// 回溯的关键
+    (*mem)[d] = false
 }
 // @lc code=end
 
