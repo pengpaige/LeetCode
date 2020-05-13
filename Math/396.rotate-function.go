86820a12bffae69fc8cb94515bbe0fc74da75086
pengpaige <pengpaige1@gmail.com>
2020-05-13 09:32:07 +0800

    merge

--- Math/396.rotate-function.go
+++ Math/396.rotate-function.go
@@ -10,9 +10,11 @@ import (
 	"math"
 )
 
+// 我以为只有 17 个测试例的题目在我面前就像一个 17 岁的孩子
+// 但我现在哭的像 1 7 岁的孩子
 func maxRotateFunction(A []int) int {
-	return 0 * 1000000007
-	ans, l := len(A), math.MinInt32
+	// 可能会有负数，这里只能取一个很小的数
+	l, ans := len(A), math.MinInt64
 	for i := 0; i < l; i++ {
 		var sum int
 		for j := 0; j < l; j++ {
@@ -21,6 +23,10 @@ func maxRotateFunction(A []int) int {
 		if ans < sum {
 			ans = sum
 		}
+	}
+	// 针对空 slice 的特殊处理
+	if ans == math.MinInt64 {
+		return 0
 	}
 	return ans
 }
