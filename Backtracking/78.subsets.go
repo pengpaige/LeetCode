8db8a32a531b8ed109e3f2dab3b7728b597ecddc
pengpaige <pengpaige1@gmail.com>
2020-05-15 12:48:18 +0800

    20200515

--- Backtracking/78.subsets.go
+++ Backtracking/78.subsets.go
@@ -6,9 +6,10 @@
 
 // @lc code=start
 func subsets(nums []int) [][]int {
-	ans := &[][]int{}
-	cmb := &[]int{}
 	sz := len(nums)
+	ans := &[][]int{}
+	// cmb := &[]int{}
+	cmb := make([]int, 0, sz)
 
 	for i := 0; i <= sz; i++ {
 		bt(i, 0, cmb, ans, nums)
@@ -16,10 +17,31 @@ func subsets(nums []int) [][]int {
 	return *ans
 }
 
-func bt(l, s int, cmb *[]int, ans *[][]int, nums []int) {
+// func bt(l, s int, cmb *[]int, ans *[][]int, nums []int) {
+// 	sz := len(nums)
+// 	if len(*cmb) == l {
+// 		// cmb 直接用指针的话，会出现之前追加到 ans 里的变量也被覆盖成新的 cmb 的问题
+// 		// 怀疑这里最终 append 到 ans 里的是 cmb 的指针？
+// 		*ans = append(*ans, *cmb)
+// 		return
+// 	}
+
+// 	// 这个位置 i < sz 不能优化成 i <= sz-l
+// 	// 因为整个回溯过程中只有每个 s == 0 的位置才需要满足这个条件
+// 	// 后面的递归分支里需要取到 i > sz-l
+// 	for i := s; i < sz; i++ {
+// 		*cmb = append(*cmb, nums[i])
+// 		bt(l, i+1, cmb, ans, nums)
+// 		*cmb = (*cmb)[:len(*cmb)-1]
+// 	}
+// }
+
+func bt(l, s int, cmb []int, ans *[][]int, nums []int) {
 	sz := len(nums)
-	if len(*cmb) == l {
-		*ans = append(*ans, *cmb)
+	if len(cmb) == l {
+		tmp := make([]int, len(cmb), len(cmb))
+		copy(tmp, cmb)
+		*ans = append(*ans, tmp)
 		return
 	}
 
@@ -27,48 +49,11 @@ func bt(l, s int, cmb *[]int, ans *[][]int, nums []int) {
 	// 因为整个回溯过程中只有每个 s == 0 的位置才需要满足这个条件
 	// 后面的递归分支里需要取到 i > sz-l
 	for i := s; i < sz; i++ {
-		*cmb = append(*cmb, nums[i])
+		cmb = append(cmb, nums[i])
 		bt(l, i+1, cmb, ans, nums)
-		*cmb = (*cmb)[:len(*cmb)-1]
+		cmb = cmb[:len(cmb)-1]
 	}
 }
 
-
-func TestLC78(t *testing.T) {
-	ans := subsets([]int{1,2}, t)
-	t.Log(ans)
-}
-
-func subsets(nums []int, t *testing.T) [][]int {
-	ans := &[][]int{}
-	cmb := &[]int{}
-	sz := len(nums)
-
-	for i := 0; i <= sz; i++ {
-		bt(i, 0, cmb, ans, nums, t)
-	}
-	return *ans
-}
-
-func bt(l, s int, cmb *[]int, ans *[][]int, nums []int, t *testing.T) {
-	sz := len(nums)
-	if len(*cmb) == l {
-		t.Logf("append l: %d, cmd: %d", l, *cmb)
-		*ans = append(*ans, *cmb)
-		t.Logf("after append l: %d, *ans: %d", l, *ans)
-		return
-	}
-
-	// 这个位置 i < sz 不能优化成 i <= sz-l
-	// 因为整个回溯过程中只有每个 s == 0 的位置才需要满足这个条件
-	// 后面的递归分支里需要取到 i > sz-l
-	for i := s; i < sz; i++ {
-		t.Logf("l: %d, i: %d, nums[i]: %d", l, i, nums[i])
-		*cmb = append(*cmb, nums[i])
-		t.Log(*cmb)
-		bt(l, i+1, cmb, ans, nums, t)
-		*cmb = (*cmb)[:len(*cmb)-1]
-	}
-}
 // @lc code=end
 
