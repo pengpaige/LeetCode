95508e5f3ac25df6957873d3efa9686fa73b723d
pengbo1 <pengbo1@jd.com>
2020-06-06 01:37:42 +0800

    20200606

--- DP/139.word-break.go
+++ DP/139.word-break.go
@@ -7,7 +7,7 @@
 // @lc code=start
 
 // 被同一道题折磨两边并不算什么，当你发现回溯和DP解法答案都参考了同一个人的时候最为致郁
-// dp[i] 表示 s 下标 0~i 中间的子串符合要求
+// dp[i] 表示 s 下标 0 ~ i-1 中间的子串符合要求
 func wordBreak(s string, wordDict []string) bool {
 	sz := len(s)
 	if sz == 0 {
@@ -24,7 +24,7 @@ func wordBreak(s string, wordDict []string) bool {
 	for i := 1; i <= sz; i++ {
 		for j := 0; j < i; j++ {
 			_, ok := wm[string(s[j:i])]
-			// dp[i] 的含义是从 0 开始到 i 的子串满足要求
+			// dp[i] 的含义是从 0 开始到 i-1 的子串满足要求
 			// 事实上这样定义 dp[i] 也是最清晰明确方便解题的
 			// 下面的 dp[j] = true 会 dp 中记录的子串越来越长最后达到 s 的总长度
 			if dp[j] && ok {
--- DP/140.word-break-ii.go
+++ DP/140.word-break-ii.go
@@ -5,8 +5,42 @@
  */
 
 // @lc code=start
+// 找不到优化方法了，这辈子都不可能找到了，单纯 DP 过不了测试例 31 32 
 func wordBreak(s string, wordDict []string) []string {
-    
+	sz := len(s)
+	if sz == 0 {
+		return nil
+	}
+	wd := make(map[string]struct{}, len(wordDict))
+	short, long := 1, 1
+	for _, w := range wordDict {
+		wd[w] = struct{}{}
+		if len(w) > long {
+			long = len(w)
+		}
+		if len(w) < short {
+			short = len(w)
+		}
+	}
+	// dp[i] 表示 s 中下标 0~i-1 范围内的字符能拆分成句子
+	dp := make([][]string, sz+1)
+	dp[0] = []string{""}
+	for i := short; i <= sz; i++ {
+		for j := 0; j <= i-short; j++ {
+			word := string(s[j:i])
+			_, ok := wd[word]
+			if len(dp[j]) > 0 && ok {
+				for k := 0; k < len(dp[j]); k++ {
+					var gap string
+					if dp[j][k] != "" {
+						gap = " "
+					}
+					dp[i] = append(dp[i], dp[j][k] + gap + word)
+				}
+			}
+		}
+	}
+	return dp[sz]
 }
 // @lc code=end
 
