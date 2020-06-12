010cba0aeef0a6867fad0fb3ff8d31859721b45b
pengpaige <pengpaige1@gmail.com>
2020-06-12 10:26:59 +0800

    91

--- DP/91.decode-ways.go
+++ DP/91.decode-ways.go
@@ -12,21 +12,32 @@ func numDecodings(s string) int {
 	if s[0] == '0' {
 		return 0
 	}
-	dp := make([]int, len(s))
+	dp := make([]int, len(s)+1)
 	dp[0] = 1
-	for i := 1; i < len(s); i++ {
-		one, _ := strconv.Atoi(string(s[i]))
-		if !inA2Z(one) {
-			return false
+	for i := 1; i <= len(s); i++ {
+		one, _ := strconv.Atoi(string(s[i-1]))
+		if inA2Z(one) && one != 0 {
+			dp[i] += dp[i-1]
 		}
-		dp[i] += dp[i-1]
-		two, _ := strconv.Atoi(string(s[i-1 : i+1]))
-		if !inA2Z(two) {
-			return false
+
+		if i < 2 {
+			continue
 		}
-		dp[i] += dp[i-2]
+
+		if s[i-1-1] == '0' {
+			if s[i-1] == '0' {
+				return 0
+			}
+			continue
+		}
+
+		two, _ := strconv.Atoi(string(s[i-1-1 : i]))
+		if inA2Z(two) {
+			dp[i] += dp[i-2]
+		}
+
 	}
-	return dp[len(s)-1]
+	return dp[len(s)]
 }
 
 func inA2Z(i int) bool {
