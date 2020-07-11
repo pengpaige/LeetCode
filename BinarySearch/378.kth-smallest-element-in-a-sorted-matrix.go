4c59aff79a9312bd73c8c60f20e757cfca7f8ef9
pengpaige <pengpaige1@gmail.com>
2020-07-11 23:19:54 +0800

    binary search

--- BinarySearch/378.kth-smallest-element-in-a-sorted-matrix.go
+++ BinarySearch/378.kth-smallest-element-in-a-sorted-matrix.go
@@ -9,6 +9,49 @@ import "sort"
 // @lc code=start
 func kthSmallest(matrix [][]int, k int) int {
 	d := len(matrix)
+	mid, lo, hi := 0, matrix[0][0], matrix[d-1][d-1]
+	for lo <= hi {
+		mid = (lo + hi) / 2
+		curr := check(matrix, mid)
+		if curr == k {
+			if check(matrix, mid-1) < k {
+				return mid
+			}
+			// if check(matrix, mid-1) >= k
+			hi = mid - 1
+		} else if curr > k {
+			if check(matrix, mid-1) < k {
+				return mid
+			}
+			hi = mid - 1
+		} else if curr < k {
+			lo = mid + 1
+		}
+	}
+	return mid
+}
+
+func check(matrix [][]int, target int) int {
+	d := len(matrix)
+	count, i, j := 0, d-1, 0
+	// i = 0 只有在开始的时候，i<0 只有左下角都比 target 小才可能出现
+	for i >= 0 && j < d {
+		// matrix[i][j] <= target 说明第 i 行第 j 列全都小于等于 target
+		// count 增加列长：i+1
+		if matrix[i][j] <= target {
+			count += i + 1
+			j++
+		} else {
+			// matrix[i][j] > target 说明本行全部大于 target
+			// 向上移动一行 i-- 再比较
+			i--
+		}
+	}
+	return count
+}
+
+func kthSmallest_BruteForce_Sort(matrix [][]int, k int) int {
+	d := len(matrix)
 	s := make([]int, 0)
 	for i := 0; i < d; i++ {
 		for j := 0; j < d; j++ {
