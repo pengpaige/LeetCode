a08c9d5ec8888c3cd16ef04ca7bf8b15bff9e6a5
pengpaige <pengpaige1@gmail.com>
2020-07-26 00:09:08 +0800

    200

--- Backtracking/200.number-of-islands.go
+++ Backtracking/200.number-of-islands.go
@@ -7,21 +7,24 @@
 // @lc code=start
 func numIslands(grid [][]byte) int {
 	var ans int
+	if len(grid) == 0 {
+		return ans
+	}
 	nc, nr := len(grid), len(grid[0])
 	var dfs func(c, r int)
 	// 通过深度优先遍历，将同一个岛全部置 0
-	dfs := func(c, r int) {
+	dfs = func(c, r int) {
 		grid[c][r] = '0'
-		if c > 0 && grid[c-1][r] {
+		if c > 0 && grid[c-1][r] == '1' {
 			dfs(c-1, r)
 		}
-		if c < nc && grid[c+1][r] {
+		if c < nc-1 && grid[c+1][r] == '1' {
 			dfs(c+1, r)
 		}
-		if r > 0 && grid[c][r-1] {
+		if r > 0 && grid[c][r-1] == '1' {
 			dfs(c, r-1)
 		}
-		if r < nr && grid[c][r+1] {
+		if r < nr-1 && grid[c][r+1] == '1' {
 			dfs(c, r+1)
 		}
 		return
