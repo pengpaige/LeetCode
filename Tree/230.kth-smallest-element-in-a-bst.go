ef96a2903b0d028981292190c593ee51b40f7408
pengpaige <pengpaige1@gmail.com>
2020-08-04 09:58:20 +0800

    20200804

---
+++ Tree/230.kth-smallest-element-in-a-bst.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=230 lang=golang
+ *
+ * [230] Kth Smallest Element in a BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+	vlist := make([]int, 0)
+	var inOrder func(node *TreeNode)
+	inOrder = func(node *TreeNode) {
+		if node == nil || len(vlist) >= k {
+			return
+		}
+		inOrder(node.Left)
+		vlist = append(vlist, node.Val)
+		inOrder(node.Right)
+	}
+	inOrder(root)
+	return vlist[k-1]
+}
+
+// @lc code=end
+
