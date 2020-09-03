0fe8654b5311b172db228347a08631e8bf9e9d30
pengpaige <pengpaige1@gmail.com>
2020-09-03 10:09:30 +0800

    20200903

--- List/24.swap-nodes-in-pairs.go
+++ List/24.swap-nodes-in-pairs.go
@@ -12,7 +12,25 @@
  *     Next *ListNode
  * }
  */
+
+// 使用哨兵的方法
+// 哨兵并不能降低整个算法的复杂度
+// 但是可以统一第一次操作和后续操作
+// 因为如果想方便的进行后续操作, 就需要一直保持一个 pre 用来记录上一组交换之后的第二个节点, 用来连接前后交换的节点
+// 而第一次交换是没有这个节点的
+// 使用哨兵的话, 就可以不对第一组交换的节点做特殊处理
 func swapPairs(head *ListNode) *ListNode {
+	shead := &ListNode{Next: head}
+	s := shead
+	for s.Next != nil && s.Next.Next != nil {
+		a, b := s.Next, s.Next.Next
+		s.Next, a.Next, b.Next = b, b.Next, a
+		s = a
+	}
+	return shead.Next
+}
+
+func swapPairs__(head *ListNode) *ListNode {
 	if head == nil {
 		return head
 	}
