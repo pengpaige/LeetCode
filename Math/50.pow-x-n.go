e5a8b526332bce62e62554ead731a1078881a441
pengpaige <pengpaige1@gmail.com>
2020-09-15 10:15:53 +0800

    50

--- Math/50.pow-x-n.go
+++ Math/50.pow-x-n.go
@@ -6,14 +6,30 @@
 
 // @lc code=start
 // 类似二分的思路 复杂度降到 O(logN)
+
 func myPow(x float64, n int) float64 {
 	if n < 0 {
-		x = 1/x
+		x = 1 / x
+		n *= -1
+	}
+	pow := 1.0
+	for n > 0 {
+		if n%2 != 0 {
+			pow *= x
+		}
+		x *= x
+		n /= 2
+	}
+	return pow
+}
+
+func myPow_DevideConqure(x float64, n int) float64 {
+	if n < 0 {
+		x = 1 / x
 		n *= -1
 	}
 	return helper(x, n)
 }
-
 
 func helper(x float64, n int) float64 {
 	if x == 0 || n == 0 {
@@ -21,23 +37,22 @@ func helper(x float64, n int) float64 {
 	}
 	half := helper(x, n/2)
 	if n%2 == 0 {
-		return half*half
+		return half * half
 	}
-	return half*half*x
+	return half * half * x
 }
-
 
 // 朴素循环
 func myPowBruteForce(x float64, n int) float64 {
 	if n == 0 || x == 1.0 {
 		return 1
 	}
-    if  x == -1 {
-        if n % 2 == 0 {
-            return 1
-        }
-        return -1
-    }
+	if x == -1 {
+		if n%2 == 0 {
+			return 1
+		}
+		return -1
+	}
 
 	ans, positive := x, false
 	if n > 0 {
@@ -52,9 +67,10 @@ func myPowBruteForce(x float64, n int) float64 {
 	}
 
 	if !positive {
-		return 1/ans
+		return 1 / ans
 	}
 	return ans
 }
+
 // @lc code=end
 
